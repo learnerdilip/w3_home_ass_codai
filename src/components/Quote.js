a72dcc424d6549ca0dcce32af8a234f75b699108
learnerdilip <diliptalks@gmail.com>
2020-01-25 12:08:46 +0100

    the liking is enabled

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,12 +1,24 @@
 import React, { Component } from "react";
 
 export default class Quote extends Component {
+  state = {
+    likes: 0,
+    dislikes: 0
+  };
+  handleLikes = () => {
+    console.log("you liked");
+  };
+
+  handleDislikes = () => {
+    console.log("you disliked");
+  };
   render() {
     return (
       <div>
-        {/* <h2>individul Quote will come here</h2> */}
         <h3>{this.props.quoteText}</h3>
         <h5>{this.props.quoteAuthor}</h5>
+        <button onClick={this.handleLikes}>:)</button>
+        <button onClick={this.handleDislikes}>:(</button>
       </div>
     );
   }
