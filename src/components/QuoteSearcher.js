91c7c803c628dc324901eac7a45db2fe57589c88
learnerdilip <diliptalks@gmail.com>
2020-01-25 11:52:46 +0100

    working fetch api

--- src/components/QuoteSearcher.js
+++ src/components/QuoteSearcher.js
@@ -3,28 +3,25 @@ import Quote from "./Quote";
 
 export default class QuoteSearcher extends Component {
   state = {
-    quotes: [
-      {
-        _id: "5d91b45d9980192a317c8acc",
-        quoteText:
-          "Notice that the stiffest tree is most easily cracked, while the bamboo or willow survives by bending with the wind.",
-        quoteAuthor: "Bruce Lee"
-      },
-      {
-        _id: "5d91b45d9980192a317c8abe",
-        quoteText:
-          "Give me six hours to chop down a tree and I will spend the first four sharpening the axe.",
-        quoteAuthor: "Abraham Lincoln"
-      },
-      {
-        _id: "5d91b45d9980192a317c8955",
-        quoteText:
-          "Good timber does not grow with ease; the stronger the wind, the stronger the trees.",
-        quoteAuthor: "J. Willard Marriott"
-      }
-    ]
+    quotes: []
   };
+
+  componentDidMount() {
+    console.log("I am working");
+    fetch("https://quote-garden.herokuapp.com/quotes/search/tree")
+      .then(response => response.json())
+      .then(data => {
+        const quoData = data.results;
+        console.log("DATA", quoData);
+        this.setState({
+          quotes: quoData
+        });
+      })
+      .catch(err => Error);
+  }
+
   render() {
+    console.log("THE LATEST STATE IS:", this.state.quotes);
     const quoteList = this.state.quotes.map(quote => (
       <Quote
         key={quote.id}
